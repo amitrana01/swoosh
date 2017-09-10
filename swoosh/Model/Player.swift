901c45a031e5fdd2fec30728c634fa412b24b3d3
Amit Rana <amit@netcoptech.com>
2017-09-10 22:24:27 +0530

    First Commit

---
+++ swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Amit Rana on 10/09/17.
+//  Copyright © 2017 netcop. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
---
+++ swoosh/View/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  swoosh
+//
+//  Created by Amit Rana on 10/09/17.
+//  Copyright © 2017 netcop. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
