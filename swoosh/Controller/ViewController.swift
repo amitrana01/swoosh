08cac838c0ae0f6da1768063ab8c99f91bd914ff
Amit Rana <amitrana01@gmail.com>
2017-09-17 20:03:48 +0530

    My First commit

---
+++ swoosh/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Amit Rana on 10/09/17.
+//  Copyright © 2017 netcop. All rights reserved.
+//
+
+import UIKit
+
+class WelcomVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+}
+
